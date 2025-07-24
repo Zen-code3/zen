76066c1f550db5d70965c6ebf57a6b20cab8c6bc
Zen-code3 <zenrickreconalla12@gmail.com>
2025-07-23 23:34:22 -0700

    message

---
+++ src/zen/act3.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zen;
+
+/**
+ *
+ * @author PC 17
+ */
+public class act3 {
+    
+}
